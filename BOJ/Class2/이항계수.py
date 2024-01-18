bfe30bd6f9a1c2ca05769f496f9513e32caf14d6
Junyong <jypark93@kookmin.ac.kr>
2024-01-18 16:14:36 +0900

    class2

---
+++ BOJ/Class2/이항계수.py
@@ -0,0 +1,15 @@
+import sys
+# sys.stdin = open("BOJ/Class2/input.txt")
+
+def factorial(N):
+    if N <= 1:
+        return 1
+    else:
+        return (factorial(N-1) * N)
+    
+N, K = map(int, sys.stdin.readline().split())
+if K == 0:
+    print(1)
+    sys.exit()
+if K<=N:
+    print(factorial(N) // (factorial(K)*factorial(N-K)))    
