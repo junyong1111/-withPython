334a969737fb1c7b9080f82a4fe5e6e7481afcce
Junyong <jypark93@kookmin.ac.kr>
2024-01-29 19:31:02 +0900

    class2 마무리

--- BOJ/Class2/solved_ac.py
+++ BOJ/Class2/solved_ac.py
@@ -1,9 +1,24 @@
 import sys
 import math
-sys.stdin = open("BOJ/Class2/input.txt")
+# sys.stdin = open("BOJ/Class2/input.txt")
 
+def round(n):
+    if n - int(n) >= 0.5:
+        return math.ceil(n)
+    else:
+        return int(n)
+    
 n = int(sys.stdin.readline())
-levels = [0] * 31
-for _ in range(n):
-    levels[(int(sys.stin))]+=1
+if n==0:
+    print(0)
+    sys.exit()
+levles = []
+for i in range(n):
+    levles.append(int(sys.stdin.readline()))
 
+top = round((n*0.15) )
+bottom = round((n*0.15))
+
+levles.sort()
+answer = levles[bottom:(n-top)]
+print(round((sum(answer)/(n-(top+bottom)))))
