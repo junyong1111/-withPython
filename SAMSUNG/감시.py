094af0538fd61b5225beb746e69f9b116fa4adb0
Junyong <jypark93@kookmin.ac.kr>
2024-03-31 22:36:03 +0900

    SAMSUNG

--- SAMSUNG/감시.py
+++ SAMSUNG/감시.py
@@ -1,33 +1,45 @@
 import sys
 import copy
-sys.stdin = open("SAMSUNG/input.txt")
+# sys.stdin = open("SAMSUNG/input.txt")
 input = sys.stdin.readline
 
+def myprint(arr, n, m, cctv):
+    for i in range(n):
+        for j in range(m):
+            print(arr[i][j], end=" ")
+        print()
+    print(" == == == ")
+    
+    print(cctv)
+    print("=== MY PRINT === ")
+
 N, M = map(int, input().split())
+
 office = []
 CCTV = []
+
 for i in range(N):
     office.append(list(map(int, input().split())))
 
+
 for i in range(N):
     for j in range(M):
-        if office[i][j] >=1 and office[i][j] <=5:
-            CCTV.append([j, i, office[i][j]])#-- x, y, CCTV 종류
-def myprint():
-    for i in range(N):
-        for j in range(M):
-            print(office[i][j], end=" ")
-        print()
-    print(CCTV)
+        if 1 <= office[i][j] < 6:
+            CCTV.append([j, i, office[i][j]])
+# myprint(office, N, M, CCTV)
+# -- init()
 
-# myprint()
-
-possibleMove = [0, 4, 2, 4, 4, 1] 
-
-def detectOneCCTV(x, y, move, newOffice):
+def detectingMap(Map, n, m):
+    ret = 0
+    for i in range(n):
+        for j in range(m):
+            if Map[i][j] == 0:
+                ret +=1
+    return ret
+                
+def ccvtNumberOne(x, y, move, newOffice):
     moving = [[(-1, 0)],[(1, 0)],[(0, -1)], [(0, 1)]]
     #-- 좌, 우, 상, 하
-    dectedSpace = 0
     
     for m in range(len(moving[move])):
         nx = x + moving[move][m][0]
@@ -36,17 +48,12 @@ def detectOneCCTV(x, y, move, newOffice):
         while 0 <= nx < M and 0 <= ny < N and newOffice[ny][nx] != 6:
             if newOffice[ny][nx] == 0:
                 newOffice[ny][nx] = -1
-                dectedSpace+=1
             nx += moving[move][m][0]
             ny += moving[move][m][1]
-            
-    return dectedSpace
 
 
-
-def detectTwoCCTV(x, y, move, newOffice):
+def ccvtNumberTwo(x, y, move, newOffice):
     moving = [[(-1, 0), (1, 0)], [(0, -1), (0, 1)]]
-    dectedSpace = 0
     for m in range(len(moving[move])):
         nx = x + moving[move][m][0]
         ny = y + moving[move][m][1]
@@ -54,16 +61,13 @@ def detectTwoCCTV(x, y, move, newOffice):
         while 0 <= nx < M and 0 <= ny < N and newOffice[ny][nx] != 6:
             if newOffice[ny][nx] == 0:
                 newOffice[ny][nx] = -1
-                dectedSpace+=1
             nx += moving[move][m][0]
             ny += moving[move][m][1]
             
-    return dectedSpace
         
-def detectThreeCCTV(x, y, move, newOffice):
+def ccvtNumberThree(x, y, move, newOffice):
     moving = [[(0, -1), (1, 0)], [(1, 0), (0, 1)], [(0, 1), (-1, 0)], [(0, -1), (-1, 0)]]
     #-- 상우, 우하, 하좌, 상좌
-    dectedSpace = 0
     for m in range(len(moving[move])):
         nx = x + moving[move][m][0]
         ny = y + moving[move][m][1]
@@ -71,16 +75,13 @@ def detectThreeCCTV(x, y, move, newOffice):
         while 0 <= nx < M and 0 <= ny < N and newOffice[ny][nx] != 6:
             if newOffice[ny][nx] == 0:
                 newOffice[ny][nx] = -1
-                dectedSpace+=1
             nx += moving[move][m][0]
             ny += moving[move][m][1]
             
-    return dectedSpace
 
-def detectFourCCTV(x, y, move, newOffice):
+def ccvtNumberFour(x, y, move, newOffice):
     moving = [[(-1, 0), (0, -1), (1, 0)], [(0, -1), (1, 0), (0, 1)], [(1, 0), (0, 1), (-1, 0)], [(0, 1), (-1, 0), (0, -1)]]
     #-- 좌상우, 상우하, 우하좌, 하좌상
-    dectedSpace = 0
     for m in range(len(moving[move])):
         nx = x + moving[move][m][0]
         ny = y + moving[move][m][1]
@@ -88,16 +89,13 @@ def detectFourCCTV(x, y, move, newOffice):
         while 0 <= nx < M and 0 <= ny < N and newOffice[ny][nx] != 6:
             if newOffice[ny][nx] == 0:
                 newOffice[ny][nx] = -1
-                dectedSpace+=1
             nx += moving[move][m][0]
             ny += moving[move][m][1]
             
-    return dectedSpace
         
-def detectFiveCCTV(x, y, move, newOffice):
+def ccvtNumberFive(x, y, move, newOffice):
     moving = [[(0, -1), (0, 1), (-1, 0), (1, 0)]]
     #-- 상하좌우
-    dectedSpace = 0
     for m in range(len(moving[move])):
         nx = x + moving[move][m][0]
         ny = y + moving[move][m][1]
@@ -105,51 +103,42 @@ def detectFiveCCTV(x, y, move, newOffice):
         while 0 <= nx < M and 0 <= ny < N and newOffice[ny][nx] != 6:
             if newOffice[ny][nx] == 0:
                 newOffice[ny][nx] = -1
-                dectedSpace+=1
             nx += moving[move][m][0]
             ny += moving[move][m][1]
             
-    return dectedSpace 
-answerList = []
-
-# x,y, cctv = CCTV[-1]
-# for move in range(possibleMove[cctv]):
-#     print(detectTwoCCTV(x, y, move))
-    
-def backtracking(index, office):
+                
+possibleCCTVmove = [0, 4, 2, 4, 4, 1]
+answer = 64
+cctvLen = len(CCTV)
+def backtracking(level, myoffice):
     global answer
-    
-    if index == len(CCTV):
-        count = 0
-        for i in range(N):
-            for j in range(M):
-                if office[i][j] == 0:
-                    count += 1
-        answer = min(answer, count)
+    if level == cctvLen:
+        ret = detectingMap(myoffice, N, M)
+        answer = min(answer, ret)
         return
+    saveOffice = copy.deepcopy(myoffice)
+    x, y, cctvnumber = CCTV[level]
     
-    x, y, cctvNumber = CCTV[index]
+    for dir in range(possibleCCTVmove[cctvnumber]):
+        if cctvnumber == 1:
+            ccvtNumberOne(x, y, dir, saveOffice)
+        elif cctvnumber == 2:
+            ccvtNumberTwo(x, y, dir, saveOffice)
+        elif cctvnumber == 3:
+            ccvtNumberThree(x, y, dir, saveOffice)
+        elif cctvnumber == 4:
+            ccvtNumberFour(x, y, dir, saveOffice)
+        elif cctvnumber == 5:
+            ccvtNumberFive(x, y, dir, saveOffice)
+        #-- 4번 
+        backtracking(level+1, saveOffice)
+        saveOffice = copy.deepcopy(myoffice)
     
-    for move in range(possibleMove[cctvNumber]):
-        newOffice = copy.deepcopy(office)
-        
-        if cctvNumber == 1:
-            detectOneCCTV(x, y, move, newOffice)
-        elif cctvNumber == 2:
-            detectTwoCCTV(x, y, move, newOffice)
-        elif cctvNumber == 3:
-            detectThreeCCTV(x, y, move, newOffice)
-        elif cctvNumber == 4:
-            detectFourCCTV(x, y, move, newOffice)
-        elif cctvNumber == 5:
-            detectFiveCCTV(x, y, move, newOffice)
-        
-        backtracking(index + 1, newOffice)
 
-startNode = 0
-visit = [False] * len(CCTV)
-visit[startNode] = True
 
-answer = float('inf')
 backtracking(0, office)
 print(answer)
+
+
+
+
