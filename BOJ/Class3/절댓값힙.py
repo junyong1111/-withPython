a2286d318aaeb419e4d4b462175949d722271c5c
Junyong <79856225+junyong1111@users.noreply.github.com>
2024-04-10 21:32:14 +0900

    Update 절댓값힙.py

--- BOJ/Class3/절댓값힙.py
+++ BOJ/Class3/절댓값힙.py
@@ -1,21 +1,19 @@
 import sys
-from queue import PriorityQueue
-# sys.stdin = open("BOJ/Class3/input.txt")
+import heapq
 
-n = int(sys.stdin.readline())
-pq = PriorityQueue()
+# sys.stdin = open("input.txt")
+input = sys.stdin.readline
 
-for _ in range(n):
-    order = int(sys.stdin.readline())
-    if order == 0 :
-        if pq.empty() == False:
-            print(pq.get()[1])
-        else:
+N = int(input())
+queue = []
+
+for _ in range(N):
+    order = int(input())
+
+    if order == 0:
+        try:
+            print(heapq.heappop(queue)[1])
+        except:
             print(0)
-        continue
-
-    if order < 0 :
-        priority = order * -1
-        pq.put((priority, order))
-        continue
-    pq.put((order, order))
+    else:
+        heapq.heappush(queue, (abs(order), order))
