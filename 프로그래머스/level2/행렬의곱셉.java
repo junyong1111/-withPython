dbfc24f8cb8a8285a40e7825098b36f39d3cdd10
Junyong <jypark93@kookmin.ac.kr>
2024-04-21 12:00:00 +0900

    프로그래머스 HINdex

---
+++ 프로그래머스/level2/행렬의곱셉.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int y = arr1.length;
+        int x = arr2[0].length;
+        int[][] answer = new int[y][x];
+        return answer;
+    }
+}
